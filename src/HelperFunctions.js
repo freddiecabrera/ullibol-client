069e9f7c2c0a600ebc264a4d173bc333223ad32a
freddiecabrera <freddiecabrera18@yahoo.com>
2016-05-14 23:22:03 -0700

    add helper function and make call to ullibol api :soccer: :running:

---
+++ src/HelperFunctions.js
@@ -0,0 +1,21 @@
+export function myEach(collection, cb) {
+    if (collection.constructor === Array) {
+      for (let i = 0; i < collection.length; i++) {
+        var index = i;
+        cb(collection[i], index, collection);
+      }
+    }
+    if (collection.constructor === Object) {
+      for (let key in collection) {
+        cb(collection[key], key, collection);
+      }
+    }
+  }
+
+export function myMap(collection, cb) {
+    let mapped = [];
+    myEach(collection, function(item) {
+      mapped.push(cb(item));
+    });
+    return mapped;
+  }
--- src/components/BallerViews.js
+++ src/components/BallerViews.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import Header from './Header'
 import { divider, MexNatFootTeam, GridView, ListView, ButtonHolder, ViewButton, EditButton } from '../../build/css/BallerViews'
+import axios from 'axios'
+import { myEach, myMap } from '../HelperFunctions'
+
+const ULLIBOL_URL = 'http://ullibolserver.herokuapp.com/allfootballers/allfootballers'
 
 const BallerViews = React.createClass({
+  getInitialState () {
+    return {
+      allfootballersData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(ULLIBOL_URL)
+      .then(response => {
+        const data = response.data
+        const newData = myMap(data, item => {
+          return JSON.parse(item)
+        })
+
+        this.setState({ allfootballersData: newData })
+      })
+      .catch(error => {
+        console.log('axios error', error)
+      })
+  },
   render () {
     document.body.style.backgroundColor = '#0e0e13'
+    const allfootballersData = this.state.allfootballersData;
+    console.log(allfootballersData);
     return (
       <div>
         <Header />
@@ -24,21 +49,26 @@ const BallerViews = React.createClass({
 
         <div className="container-fluid">
           <div className="row">
-          <ul className="collection" style={{borderColor: '#0F0E13', marginLeft: '11%', marginRight: '11%'}}>
-            <li className="collection-item avatar z-depth-4" style={{backgroundColor: '#17161D', maxWidth: '100%', overflowX: 'hidden'}}>
-              <img src="http://media.gettyimages.com/photos/andres-guardado-of-psv-during-the-dutch-eredivisie-match-between-psv-picture-id502035286" alt="" className="circle" />
-              <div style={{color: 'white'}}>
-                <span className="title">Andres Guardado</span>
-                <p>Midfielder</p>
-              </div>
-              <div style={ButtonHolder}>
-                <button style={ViewButton} className='waves-effect waves-light btn'>View</button>
-                <button style={EditButton} className='waves-effect waves-light btn'>Edit</button>
-              </div>
-            </li>
-          </ul>
+          { myMap(allfootballersData, (player) => {
+            return (
+              <ul key={player.name} className="collection" style={{borderColor: '#0F0E13', marginLeft: '11%', marginRight: '11%'}}>
+                <li className="collection-item avatar z-depth-4" style={{backgroundColor: '#17161D', maxWidth: '100%', overflowX: 'hidden'}}>
+                  <img src={player.url} alt="" className="circle" style={{width: '44px', height: '64px', borderRadius: '2%'}} />
+                  <div style={{color: 'white'}}>
+                    <span className="title">{player.name}</span>
+                    <p>{player.position}</p>
+                  </div>
+                  <div style={ButtonHolder}>
+                    <button style={ViewButton} className='waves-effect waves-light btn'>View</button>
+                    <button style={EditButton} className='waves-effect waves-light btn'>Edit</button>
+                  </div>
+                </li>
+              </ul>
+            )
+          })}
           </div>
         </div>
+
       </div>
     )
   }
